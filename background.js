8311b4b9d13107df7d4e090d2842cb60bedb6d3c
krishan <krishanmondal273@gmail.com>
2024-06-02 11:32:06 +0530

    Made pause functionality

--- background.js
+++ background.js
@@ -1,6 +1,7 @@
 const socialMediaSites = ["facebook.com", "twitter.com", "instagram.com"];
 const timeIntervals = {};
-let siteTimers = {};
+const siteTimers = {};
+const remainingTime = {};
 let activeSite = null;
 
 //Listens to chrome alarms and closes the tab
@@ -12,9 +13,18 @@ chrome.alarms.onAlarm.addListener((alarm) => {
       for (let tab of tabs) {
         if (new URL(tab.url).hostname.includes(site)) {
           chrome.tabs.remove(tab.id);
+          delete siteTimers[site];
+          delete remainingTime[site];
         }
       }
     });
+  }
+});
+
+//checks if the updated tab is a social media site
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === "complete" && tab.url) {
+    handleTab(tab);
   }
 });
 
@@ -25,34 +35,37 @@ chrome.tabs.onActivated.addListener((activeInfo) => {
   });
 });
 
-//checks if the updated tab is a social media site
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === "complete" && tab.url) {
-    handleTab(tab);
-  }
-});
-
 function handleTab(tab) {
   if (tab.url) {
     const url = new URL(tab.url);
     const hostname = url.hostname;
-    console.log("I'm working");
-    //Funtion for social media site
+
     let isSocialMediaSite = false;
+
     for (let site of socialMediaSites) {
       if (hostname.includes(site)) {
         isSocialMediaSite = true;
-        // If the active site changes, stop the timer for the previous site
+
         if (activeSite !== site) {
           stopActiveTimer(activeSite);
           chrome.alarms.clear(`limit-${activeSite}`);
           siteTimers[activeSite] = 0;
         }
         activeSite = site;
+
+        //starts the timer from the remaining time
+        if (remainingTime[site]) {
+          let limit = remainingTime[site];
+          startTimer(site, limit);
+          return;
+        }
+
         //gets the limit of a site from storage
         chrome.storage.sync.get(["siteLimits"], (result) => {
-          const { siteLimits } = result || {};
+          const siteLimits = result.siteLimits || {};
           let limit = siteLimits[site] || 30;
+          remainingTime[site] = limit;
+      
           if (!siteTimers[site]) {
             siteTimers[site] = 0;
           }
@@ -79,17 +92,18 @@ function startTimer(site, limit) {
   }
   timeIntervals[site] = setInterval(() => {
     siteTimers[site]++;
-    console.log(`Time spent on ${site}: ${siteTimers[site]} minutes`);
+    remainingTime[site]--;
+
     if (siteTimers[site] >= limit) {
       chrome.alarms.create(`limit-${site}`, { delayInMinutes: 0 });
       stopActiveTimer(site);
     }
-  }, 10000); // Increment every minute
+  }, 60000);
 }
 
 function stopActiveTimer(site) {
   clearInterval(timeIntervals[site]);
-  delete timeIntervals[site]; //delete the property
+  delete timeIntervals[site];
 }
 
 //function to create chrome notification
